9ddc30c7b40261a8489f13e080bb063401d1fefc
Zaytcev <zaydisk@yandex.ru>
2020-09-01 09:26:05 +0300

    first commit

--- index.php
+++ index.php
@@ -3,15 +3,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-echo phpinfo();
-
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    //$response->getBody()->write("Hello world!");
+    //return $response;
+    echo 'Hello world!';
 });
 
-//$app->run();
+$app->run();
